b826cf2cc0022ae9d84436e091a4ab3abc85ecbc
brandonmooney1217 <brandonmooney2023@gmail.com>
2026-05-03 12:05:51 -0700

    57

---
+++ intervals/insert_interval.go
@@ -0,0 +1,33 @@
+package intervals
+
+func insert(intervals [][]int, newInterval []int) [][]int {
+
+	i := 0
+	n := len(intervals)
+	res := [][]int{}
+
+	/*
+		Add all interval that do not overlap with new interval
+	*/
+	for i < n && newInterval[0] > intervals[i][1] {
+		res = append(res, intervals[i])
+		i++
+	}
+
+	/*Merge step*/
+	for i < n && newInterval[1] >= intervals[i][0] {
+		newInterval[0] = min(newInterval[0], intervals[i][0])
+		newInterval[1] = max(newInterval[1], intervals[i][1])
+		i++
+	}
+
+	res = append(res, newInterval)
+
+	/*Add remaining interval that do not overlap with new interval*/
+
+	for i < n {
+		res = append(res, intervals[i])
+		i++
+	}
+	return res
+}
