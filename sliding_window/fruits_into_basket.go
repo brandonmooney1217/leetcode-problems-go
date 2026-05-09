e98a6da362838f84065cf341c42e43cc01999895
brandonmooney1217 <brandonmooney2023@gmail.com>
2026-05-09 08:01:41 -0700

    kok eating and fruits into basket

---
+++ sliding_window/fruits_into_basket.go
@@ -0,0 +1,33 @@
+package sliding_window
+
+// LeetCode #904 (Medium):https://leetcode.com/problems/fruit-into-baskets/
+func totalFruit(fruits []int) int {
+	res, left, count := 0, 0, 0
+	seen := make(map[int]int)
+	n := len(fruits)
+
+	for r := 0; r < n; r++ {
+		curr := fruits[r]
+		if _, ok := seen[curr]; ok {
+			seen[curr]++
+		} else {
+			seen[curr] = 1
+		}
+		count++
+
+		for len(seen) > 2 {
+			left_value := fruits[left]
+			seen[left_value]--
+			count--
+			if seen[left_value] == 0 {
+				delete(seen, left_value)
+			}
+			left++
+		}
+		if count > res {
+			res = count
+		}
+
+	}
+	return res
+}
