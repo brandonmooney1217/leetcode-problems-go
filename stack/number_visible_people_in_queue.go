84c0deb1943a67795ff5c4b0de12869fee12751e
brandonmooney1217 <brandonmooney2023@gmail.com>
2026-05-09 08:47:27 -0700

    1944

---
+++ stack/number_visible_people_in_queue.go
@@ -0,0 +1,27 @@
+package stack
+
+func canSeePersonsCount(heights []int) []int {
+
+	n := len(heights)
+	res := make([]int, n)
+
+	stack := []int{}
+
+	for i := n - 1; i >= 0; i-- {
+		viewCount := 0
+		for len(stack) > 0 && heights[i] > stack[len(stack)-1] {
+			viewCount++
+			stack = stack[:len(stack)-1]
+		}
+
+		if len(stack) > 0 {
+			viewCount++
+		}
+
+		stack = append(stack, heights[i])
+		res[i] = viewCount
+	}
+
+	return res
+
+}
