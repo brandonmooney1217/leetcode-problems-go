e1bb906ea21272ca5f824b9902b89f323a3885cc
brandonmooney1217 <brandonmooney2023@gmail.com>
2026-05-10 13:02:54 -0700

    2770

---
+++ dynamic_programming/max_number_jumps_to_reach_last_index.go
@@ -0,0 +1,32 @@
+package dynamicprogramming
+
+// https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/description/
+
+func maximumJumps(nums []int, target int) int {
+
+	n := len(nums)
+	res := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		if i == n-1 {
+			res[i] = 0
+		} else {
+			res[i] = -1
+		}
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			difference := nums[j] - nums[i]
+
+			if difference >= (-1*target) && difference <= target {
+				if res[j] != -1 {
+					res[i] = max(res[i], res[j]+1)
+				}
+			}
+		}
+	}
+
+	return res[0]
+
+}
