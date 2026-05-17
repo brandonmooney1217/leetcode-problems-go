35ad0c077fe560adbcfacf95f1cedfc00b54337b
brandonmooney1217 <brandonmooney2023@gmail.com>
2026-05-16 18:38:04 -0700

    733

---
+++ graphs/dfs/flood-fill.go
@@ -0,0 +1,33 @@
+package dfs
+
+// LeetCode #733 (Easy): https://leetcode.com/problems/flood-fill/
+
+func floodFill(image [][]int, sr int, sc int, color int) [][]int {
+
+	rows, cols := len(image), len(image[0])
+	startColor := image[sr][sc]
+
+	if startColor == color {
+		return image
+	}
+
+	var dfs func(r, c int)
+	dfs = func(r, c int) {
+		if r < 0 || r >= rows || c < 0 || c >= cols {
+			return
+		}
+		if image[r][c] != startColor {
+			return
+		}
+
+		image[r][c] = color
+		dfs(r+1, c)
+		dfs(r-1, c)
+		dfs(r, c-1)
+		dfs(r, c+1)
+	}
+
+	dfs(sr, sc)
+	return image
+
+}
