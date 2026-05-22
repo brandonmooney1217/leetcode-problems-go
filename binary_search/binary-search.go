ef19918db00aaeb7dff275686e11d6b864a01bd8
brandonmooney1217 <brandonmooney2023@gmail.com>
2026-05-21 19:31:53 -0700

    704

---
+++ binary_search/binary-search.go
@@ -0,0 +1,26 @@
+package binarysearch
+
+// LeetCode #704 (Easy): https://leetcode.com/problems/binary-search/
+
+func searchAgain(nums []int, target int) int {
+
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+
+	}
+
+	return -1
+
+}
